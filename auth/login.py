273c82c04b705d25573fa4c43c6098a59b317df4
Kl1600 <promopro01700@gmail.com>
2026-06-08 19:50:24 +0200

    login avancer avec verif de password et d'email mis en place

--- auth/login.py
+++ auth/login.py
@@ -1,20 +1,30 @@
 from api.auth_routes import recup_contenu
+from password import hash_password
 
 def login_user():
     email=str(input("Entrez votre email :"))
     password=str(input("Entrez votre mot de passe :"))
+    indice=-1
     contenu=recup_contenu()
     if email not in contenu:
         print("vous n'avez pas de compte")
     else:
-        verify_password(password)
-        if verify_password(password)==True:
-            print ("connection avec succès !")
+        for i in contenu:
+            indice=indice+1
+            if contenu([indice]['email'])==email:
+                password_hasher=hash_password(password)
+                if contenu([indice]['mdp'])==password_hasher:
+                    print("Connexion avec succès !!")
+                    load_user()
         
 
 
 def verify_password(password):
-    pass
+    contenu=recup_contenu()
+    if password in contenu:
+        pass
+        
 
 def load_user():
     pass
+    
