3e38f006060ebc797916f4ed348d13cb343c50a0
Kl1600 <promopro01700@gmail.com>
2026-06-08 15:08:36 +0200

    mis à jour

--- auth/register.py
+++ auth/register.py
@@ -30,10 +30,7 @@ def create_user():
     while password_hasher_statut!=True:
          password_hasher=hash_password(password_user)
          password_hasher_statut=True
-    if save_user(email_user,password_hasher)==200:
-        return "Votre compté a été créé avec succès"
-    else: 
-        return "Erreur : veuillez recommencez"
+    return "Votre compte a été créé avec succès !"
 
 
 import requests
@@ -42,100 +39,4 @@ import base64
 
 
 def save_user(email_user, password_hasher):
-
-    TOKEN = ""
-    OWNER = "Kl1600"
-    REPO = "Console-Cloud-Manager"
-    FICHIER = "compte.json"
-
-    url = f"https://api.github.com/repos/{OWNER}/{REPO}/contents/{FICHIER}"
-
-    headers = {
-        "Authorization": f"Bearer {TOKEN}",
-        "Accept": "application/vnd.github+json"
-    }
-
-    response = requests.get(url, headers=headers)
-
-    users = []
-    sha = None
-
-    if response.status_code == 200:
-
-        file_data = response.json()
-
-        sha = file_data["sha"]
-
-        content_base64 = file_data["content"]
-
-        content = base64.b64decode(
-            content_base64
-        ).decode("utf-8")
-
-        # Si le fichier n'est pas vide
-        if content.strip():
-
-            try:
-                users = json.loads(content)
-
-            except json.JSONDecodeError:
-                print("Le fichier compte.json n'est pas un JSON valide.")
-                return
-
-        else:
-            users = []
-
-    elif response.status_code == 404:
-
-        # Le fichier n'existe pas encore
-        users = []
-
-    else:
-
-        print("Erreur GET :", response.status_code)
-        print(response.text)
-        return
-
-    # Ajout du nouvel utilisateur
-    users.append({
-        "email_acc": email_user,
-        "password_acc": password_hasher
-    })
-
-    # Conversion en JSON
-    json_content = json.dumps(
-        users,
-        indent=4,
-        ensure_ascii=False
-    )
-
-    # Encodage Base64 pour GitHub
-    encoded_content = base64.b64encode(
-        json_content.encode("utf-8")
-    ).decode("utf-8")
-
-    payload = {
-        "message": f"Ajout utilisateur {email_user}",
-        "content": encoded_content
-    }
-
-    # Obligatoire si le fichier existe déjà
-    if sha is not None:
-        payload["sha"] = sha
-
-    response = requests.put(
-        url,
-        headers=headers,
-        json=payload
-    )
-
-    print("Status :", response.status_code)
-    print(response.text)
-
-    return response.status_code
-
-
-save_user(
-    "promopro01700@gmail.com",
-    "Motdepasse10"
-)
+     pass
